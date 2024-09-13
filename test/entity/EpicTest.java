338bff4ddfeb1a8299c5fef51a8c1ac549c7f31d
Юрий А <dragonsflames@yandex.ru>
2024-09-13 16:42:52 +0300

    display name added

--- test/entity/EpicTest.java
+++ test/entity/EpicTest.java
@@ -1,11 +1,14 @@
 package entity;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Эпик")
 public class EpicTest {
     @Test
+    @DisplayName("Должен быть равен эпику с таким же ID")
     void epicsWithEqualIdShouldBeEqual() {
         Epic task1 = new Epic(0, "Pop", "Pop", TaskStatus.IN_PROGRESS);
         Epic task2 = new Epic(0, "Dop", "Gop", TaskStatus.DONE);
