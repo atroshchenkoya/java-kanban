539f81f7bba60730ced138acdc5650dd5fdd2c83
atroshchenkoya <dragonsflames@yandex.ru>
2024-11-03 02:34:00 +0300

    sp9 - test first steps

--- src/ru/practicum/kanban/servers/HttpTaskServer.java
+++ src/ru/practicum/kanban/servers/HttpTaskServer.java
@@ -12,15 +12,22 @@ public class HttpTaskServer {
     private static final int PORT = 8090;
 
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
         TaskManager taskManager = Managers.getDefault();
+        HttpTaskServer server = new HttpTaskServer();
+        server.start(taskManager);
+    }
 
+    public void start(TaskManager taskManager) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+        createServerContext(taskManager, httpServer);
+        httpServer.start();
+    }
+
+    private static void createServerContext(TaskManager taskManager, HttpServer httpServer) {
         httpServer.createContext("/tasks", new TasksHandler(taskManager));
         httpServer.createContext("/subtasks", new SubTasksHandler(taskManager));
         httpServer.createContext("/epics", new EpicsHandler(taskManager));
         httpServer.createContext("/history", new HistoryHandler(taskManager));
         httpServer.createContext("/prioritized", new PrioritizedHandler(taskManager));
-
-        httpServer.start();
     }
 }
